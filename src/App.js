cbfbe01b42f4083832cb081d82a289d49065a1b8
samuel <samuelegea@hotmail.com>
2021-05-10 22:15:08 -0300

    fix production error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Home from './pages/Home/Home'
 import Header from './components/Header/Header';
 import { Routes } from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
